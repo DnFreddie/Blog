b383cb255fd53344e0d91300576a5365d1054b3d
DnFreddie <defnotfreddie@gmail.com>
2024-05-17 20:58:21 +0200

    Get rid of the santiaze in markdown should speed up things

--- src/lib/mdRender.ts
+++ src/lib/mdRender.ts
@@ -1,9 +1,6 @@
 import { marked } from "marked";
-import pkg from "isomorphic-dompurify";
-const { sanitize } = pkg;
 
 export function santeizeM(mdString: string): string {
   const html: string = marked.parse(mdString).toString();
-  const clean: string = sanitize(html);
-  return clean;
+  return html;
 }
